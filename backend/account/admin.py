c155187119c6d4c667e666300ac22c8f8b62d641
Juwon Hwang <wndnjs719@gmail.com>
2023-09-27 18:44:33 +0900

    [feat] admin 작성

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,29 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.hashers import make_password
-from WellPlay_app.backend.account.models import User
+from .models import User
 
 
-class UserAdmin(BaseUserAdmin):
-    list_display = ('user_id', 'email', 'profile_name', 'lol_name')
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'email', 'profile_name')
     list_filter = ('user_id',)
     search_fields = ('user_id', 'profile_name')
-
-    fieldsets = ("info", {'fields': ('user_id', 'email', 'password', 'profile_name', 'lol_name')}),
-    ('Permissions', {'fields': ('is_admin', 'is_active',)},)
-
-    filter_horizontal = []
-
-    def get_readonly_fields(self, request, obj=None):
-        if obj:
-            return ('user_id',)
-        else:
-            pass
-
-    def create(self, validation_data):
-        password = validation_data.pop('password')
-        password = make_password(password)
-        validation_data['password'] = password
-
-
-admin.site.register(User, UserAdmin)
+    ordering = ('user_id',)
