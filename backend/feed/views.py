cc6faca24187d1db3909038bf4b8c6248b2cb43b
bainaryho <rkswl233@naver.com>
2023-09-27 17:45:09 +0900

    [feat] Add Comment CommentSerializer&viewset

--- backend/feed/views.py
+++ backend/feed/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
-from .serializers import FeedSerializer
+from .serializers import FeedSerializer, CommentSerializer
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.decorators import action
@@ -33,25 +33,30 @@ class FeedViewset(viewsets.ModelViewSet):
     def destroy(self, request, pk=None):
         pass
 
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Feed.objects.all()
+    serializer_class = CommentSerializer
+
     # 댓글 불러오기
     @action(detail=True, methods=["get"])
     def comments(self, request, pk=None):
         pass
 
-    # 댓글 입력
-    @action(detail=True, methods=["post", "put", "delete"])
+    # 댓글 입력, 수정, 삭제
+    @action(detail=True, methods=["post"])
     def comment(self, request, pk=None):
         pass
 
-    # # 댓글 수정
-    # @action(detail=True, methods=["put"])
-    # def comment_update(self, request, pk=None):
-    #     pass
+    # 댓글 수정
+    @action(detail=True, methods=["put"])
+    def comment(self, request, pk=None):
+        pass
 
-    # # 댓글 삭제
-    # @action(detail=True, methods=["delete"])
-    # def comment_delete(self, request, pk=None):
-    #     pass
+    # 댓글 삭제
+    @action(detail=True, methods=["delete"])
+    def comment(self, request, pk=None):
+        pass
 
     @action(detail=True, methods=["post"])
     def like(self, request, pk=None):
