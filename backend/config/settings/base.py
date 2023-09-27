c56af8c9297cc174b3a0ebdebe4b499363e8dc46
Juwon Hwang <wndnjs719@gmail.com>
2023-09-27 18:44:51 +0900

    [set] 설정에 AUTH_USER_MODEL 추가

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -85,11 +85,8 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB", "wellplay"),
-        "USER": os.getenv("POSTGRES_USER", "postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
-        "HOST": os.getenv("DB_HOST", "db"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
