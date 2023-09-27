d8c21e54f41e6c7c263d835f83ad07467b6a840c
Juwon <81235429+prace-prc@users.noreply.github.com>
2023-09-27 18:48:57 +0900

    [feat] user model (#23)
    * [feat] 커스텀 유저 모델을 위한 UserManager 클래스 생성
    
    * [feat] UserManager 클래스에 superuser 생성 함수 추가
    
    * [feat] User 클래스 작성중
    
    * [docs] 커밋 메시지 템플릿 수정
    
    * [feat] User 클래스 작성 완료
    
    * [feat] 어드민 작성
    
    * [set] 인증 모델로 커스텀 유저 모델 지정
    
    * [docs] .gitignore에 migrations 폴더 파일 추가
    
    * [docs] requirements에 Pillow 추가
    
    * [feat] feed 모델에서 User 부분 settings.AUTH_USER_MODEL로 변경
    
    * [fix] 유저 모델 작성 수정
    
    * [feat] admin 작성
    
    * [set] 설정에 AUTH_USER_MODEL 추가

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'email', 'profile_name')
+    list_filter = ('user_id',)
+    search_fields = ('user_id', 'profile_name')
+    ordering = ('user_id',)
