fc0341fcc3e5b5deb28b19b8658424a22ef28dd4
Juwon Hwang <wndnjs719@gmail.com>
2023-09-27 11:42:52 +0900

    [feat] 커스텀 유저 모델을 위한 UserManager 클래스 생성

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 
+
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, user_id, email, profile_name, password=None):
+        if not user_id:
+            raise ValueError('ID를 입력해야 합니다.')
+        user = self.model(
+            email=email,
+            user_id=user_id,
+            profile_name=profile_name,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
