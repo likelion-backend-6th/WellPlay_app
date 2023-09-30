c6de8b7e1314a7aba2f01894a51f3d87090e994a
Juwon Hwang <wndnjs719@gmail.com>
2023-09-30 21:40:16 +0900

    [feat] 회원가입 API 뷰 작성

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import jwt
+from django.urls import path, include
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from .serializers import *
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import status, viewsets, routers
+from rest_framework.response import Response
 
-# Create your views here.
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+
+            token = TokenObtainPairSerializer.get_token(user)
+            refresh_token = str(token)
+            access_token = str(token.access_token)
+            res = Response(
+                {
+                    "user": serializer.data,
+                    "message": "register successs",
+                    "token": {
+                        "access": access_token,
+                        "refresh": refresh_token,
+                    },
+                },
+                status=status.HTTP_200_OK,
+            )
+
+            res.set_cookie("access", access_token, httponly=True)
+            res.set_cookie("refresh", refresh_token, httponly=True)
+
+            return res
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
