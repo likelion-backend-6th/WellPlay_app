bcdc835f16f71c916f948ae2c0968f924ff4afec
Juwon <81235429+prace-prc@users.noreply.github.com>
2023-10-01 10:14:26 +0900

    [feat] api register (#28)
    * [feat] 커스텀 유저 모델을 위한 UserManager 클래스 생성
    
    * [feat] UserManager 클래스에 superuser 생성 함수 추가
    
    * [set] RESTFramework simplejwt 사용 위한 설정 추가
    
    * [fix] Pycharm에서 오류 발생하는 import 구문 수정
    
    * [feat] 유저 시리얼라이저 작성
    
    * [fix] 오타 수정
    
    * [set] 회원가입 기능 테스트 위해 rest framework 권한 AllowAny로 변경
    
    * [fix] import 구문 오류 수정
    
    * [feat] 시리얼라이저에 user_id 필드 추가
    
    * [feat] 프로젝트 config urls.py에 account url 추가
    
    * [feat] 회원가입 API 뷰 작성
    
    * [feat] 회원가입 API URL 작성
    
    ---------
    
    Co-authored-by: binary_ho <90160898+bainaryho@users.noreply.github.com>

---
+++ backend/account/serializers.py
@@ -0,0 +1,16 @@
+from .models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            user_id=validated_data['user_id'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
