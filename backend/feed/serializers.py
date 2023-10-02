5423241d028064b87b7c991eb4936c2f814bfae9
JunYoung_Kim <43990500+hjklo0220@users.noreply.github.com>
2023-10-02 12:59:24 +0900

    [feat] feed view v0.1 (#31)
    * [feat] Comment viewset urls route
    
    * [refactor] model refactoring
    
    * [feat] feed view v0.1

--- backend/feed/serializers.py
+++ backend/feed/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Feed, Comment, Notification
+from .models import Feed, Comment, Notification, Like
 
 
 class FeedSerializer(serializers.ModelSerializer):
@@ -12,6 +12,20 @@ class FeedSerializer(serializers.ModelSerializer):
             "created_at",
             "updated_at",
         )
+
+
+class FeedUploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feed
+        fields = [
+            "content",
+            "image",
+            "video",
+        ]
+        read_only_fields = ("owner", "created_at", "updated_at")
+
+    image = serializers.ImageField(use_url=True)
+    video = serializers.FileField(use_url=True)
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -27,6 +41,13 @@ class CommentSerializer(serializers.ModelSerializer):
         )
 
 
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = "__all__"
+        read_only_fields = ("user", "feed", "created_at")
+
+        
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
