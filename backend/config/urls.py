57b791ebe5268c68a8d2dec9bdc57918f3cb1e65
JunYoung_Kim <hjklo0220@gmail.com>
2023-10-02 13:22:36 +0900

    [feat] add jwt api in docs

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,6 +3,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 from django.urls import include
 
 from feed.urls import feed_router, comment_router
@@ -11,9 +16,13 @@ from feed.urls import feed_router, comment_router
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth", include("rest_framework.urls")),
-    path("account/", include('account.urls')),
+    path("account/", include("account.urls")),
     path("feed/", include(feed_router.urls)),
     path("comment/", include(comment_router.urls)),
+    # jwt
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     # drf-spectacular
     path("api/schema/", SpectacularAPIView.as_view(), name="api-schema"),
     path(
