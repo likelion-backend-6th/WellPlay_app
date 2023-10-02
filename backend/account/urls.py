f6dfb726dd99b9bf5985f491a9cbfca2864daf5b
Juwon Hwang <wndnjs719@gmail.com>
2023-10-02 20:29:25 +0900

    [feat] 로그인, 로그아웃, 토큰 리프레시 url 추가

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from account.views import RegisterAPIView
+from account.views import RegisterAPIView, LoginAPIView, LogoutAPIView
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view()),
+    path("login/", LoginAPIView.as_view()),
+    path("logout/", LogoutAPIView.as_view()),
+    path("refresh/", TokenRefreshView.as_view())
 ]
