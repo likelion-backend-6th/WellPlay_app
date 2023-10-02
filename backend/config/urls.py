c902a45ac714ad05cf693f47a4e43e1c8229a257
bainaryho <rkswl233@naver.com>
2023-10-02 18:01:48 +0900

    [feat] comment url 중복 수정

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -11,24 +11,18 @@ from feed.urls import feed_router, comment_router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api-auth", include("rest_framework.urls")),
+    path("api-auth/", include("rest_framework.urls")),
     path("account/", include("account.urls")),
     path("feed/", include(feed_router.urls)),
     # 특정 피드의 댓글 목록을 가져오는 URL 패턴
     path(
-        "feeds/<int:feed_id>/comments/",
-        CommentViewSet.as_view({"get": "list"}),
-        name="list-comments",
-    ),
-    # 댓글 생성을 위한 URL 패턴
-    path(
-        "feeds/<int:feed_id>/comments/",
-        CommentViewSet.as_view({"post": "create"}),
-        name="create-comment",
+        "feed/<int:id>/comments/",
+        CommentViewSet.as_view({"get": "list", "post": "create"}),
+        name="list-create-comments",
     ),
     # 댓글 수정, 삭제를 위한 URL 패턴
     path(
-        "feeds/<int:feed_id>/comments/<int:pk>/",
+        "feed/<int:id>/comments/<int:pk>/",
         CommentViewSet.as_view({"put": "update", "delete": "destroy"}),
         name="update-delete-comment",
     ),
