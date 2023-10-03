6f86d1cfa491990af4251ce3587c08d3b559447d
JunYoung_Kim <hjklo0220@gmail.com>
2023-10-03 22:55:56 +0900

    [fix] fixed comment api urls

--- backend/feed/urls.py
+++ backend/feed/urls.py
@@ -11,6 +11,16 @@ router = DefaultRouter()
 router.register(r"", views.FeedViewSet, basename="")
 
 urlpatterns = [
-    path("<int:pk>/like/", views.LikeView.as_view(), name="like"),
     path("", include(router.urls)),
+    path("<int:pk>/like/", views.LikeView.as_view(), name="like"),
+    path(
+        "<int:id>/comments/",
+        views.CommentViewSet.as_view({"get": "list", "post": "create"}),
+        name="list-create-comments",
+    ),
+    path(
+        "feed/<int:id>/comments/<int:comment_id>/",
+        views.CommentViewSet.as_view({"put": "update", "delete": "destroy"}),
+        name="update-delete-comment",
+    ),
 ]
