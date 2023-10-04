7727ac14713bda8a49c1345c12031a7eb7464ca3
bainaryho <rkswl233@naver.com>
2023-10-04 17:37:33 +0900

    [feat] frontend setting for vue

---
+++ frontend/wp-front-app/config.js
@@ -0,0 +1,5 @@
+const serverUrl = "http://localhost:8000"; // 서버 주소 설정
+
+export default {
+  serverUrl,
+};
---
+++ frontend/wp-front-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <RegisterForm></RegisterForm>
+  </div>
+</template>
+
+<script>
+import RegisterForm from "./components/RegisterForm.vue";
+
+export default {
+  components: {
+    RegisterForm,
+  },
+};
+</script>
