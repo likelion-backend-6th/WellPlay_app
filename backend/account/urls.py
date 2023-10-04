266635020624a2861cd4489245cab6b72f853e1c
Juwon Hwang <wndnjs719@gmail.com>
2023-10-04 14:10:26 +0900

    [feat] ProfileAPIView url 추가

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from account.views import RegisterAPIView, LoginAPIView, LogoutAPIView, FollowAPIView
+from account.views import RegisterAPIView, LoginAPIView, LogoutAPIView, ProfileAPIView, FollowAPIView
+
 
 urlpatterns = [
     path("register/", RegisterAPIView.as_view()),
     path("login/", LoginAPIView.as_view()),
     path("logout/", LogoutAPIView.as_view()),
     path("refresh/", TokenRefreshView.as_view()),
+    path("current/", ProfileAPIView.as_view()),
     path("follow/", FollowAPIView.as_view())
 ]
