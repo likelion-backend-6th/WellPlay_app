3db840ef330b8237d420e1d7eaec9a640e4bfbb0
bainaryho <rkswl233@naver.com>
2023-10-05 14:14:13 +0900

    [set] delete cors setting

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -57,7 +57,6 @@ INSTALLED_APPS += [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -196,10 +195,3 @@ CELERY_BROKER_URL = "redis://localhost:6379/0"  # Redis 메시지 브로커 URL
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",  # Vue.js 개발 서버 주소
-    # 다른 허용할 주소들 추가
-]
-
-CORS_URLS_REGEX = r"^.*$"  # 우선 url은 전부 열어놓을게요
