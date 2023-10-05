ebafcbc9d874738f6fb489fc4659a654114091fe
Juwon Hwang <wndnjs719@gmail.com>
2023-10-05 13:57:37 +0900

    [feat] 프로필 url 세분화, 다른 유저 프로필 url 추가

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from account.views import RegisterAPIView, LoginAPIView, LogoutAPIView, ProfileAPIView, FollowAPIView, FollowingList, FollowerList
+from account.views import RegisterAPIView, LoginAPIView, LogoutAPIView, ProfileAPIView, FollowAPIView, FollowingList, FollowerList, UserProfileAPIView
 
 
 urlpatterns = [
@@ -12,5 +12,6 @@ urlpatterns = [
     path("follow/", FollowAPIView.as_view()),
     path("following/", FollowingList.as_view()),
     path("follower/", FollowerList.as_view()),
-    path("current/", ProfileAPIView.as_view()),
+    path("profile/current/", ProfileAPIView.as_view()),
+    path("profile/<str:user_id>/", UserProfileAPIView.as_view())
 ]
