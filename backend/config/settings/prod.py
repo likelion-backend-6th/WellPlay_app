7a87e1fa820485b3d361dc415a2a35407526ca98
JunYoung_Kim <hjklo0220@gmail.com>
2023-10-06 11:37:25 +0900

    [feat] add db setting, add STATIC_ROOT

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -3,3 +3,13 @@ from .base import *
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "postgres"),
+        "USER": os.getenv("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
+        "HOST": os.getenv("DB_HOST", "db"),
+    }
+}
