9aa437a0d9044b94bf9da05f9f86e2718ae745ba
JunYoung_Kim <43990500+hjklo0220@users.noreply.github.com>
2023-10-06 14:43:51 +0900

    [fix] fix DEFAULT_PERMISSION_CLASSES (#67)

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -139,7 +139,7 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     # "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.AllowAny"],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
