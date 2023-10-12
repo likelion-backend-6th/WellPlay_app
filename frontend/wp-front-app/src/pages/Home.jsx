3ad385e2f90158f3692a2431202df19947eb13ad
Juwon <81235429+prace-prc@users.noreply.github.com>
2023-10-12 16:17:09 +0900

    [feat] 피드 추천순 보기 기능 추가 (#87)

--- frontend/wp-front-app/src/pages/Home.jsx
+++ frontend/wp-front-app/src/pages/Home.jsx
@@ -1,61 +1,72 @@
-import React, { useState, useEffect } from "react";
-import { Col, Image, Row } from "react-bootstrap";
+import React, {useState, useEffect} from "react";
+import {Col, Image, Row} from "react-bootstrap";
 import Layout from "../components/Layout";
 import CreateFeed from "../components/feeds/CreateFeed";
 import Feed from "../components/feeds/Feed";
 import ProfileCard from '../components/profile/ProfileCard';
 
 function Home() {
-  const [feeds, setFeeds] = useState([]);
+    const [feeds, setFeeds] = useState([]);
+    const [sortBy, setSortBy] = useState('latest');
 
-  const fetchFeeds =  async function() {
-	try {
-	  const apiUrl = process.env.REACT_APP_API_URL+"/feed/";
+    const fetchFeeds = async function () {
+        try {
+            let apiUrl;
 
-	  const response = await fetch(apiUrl, {
-		method: "GET",
-		headers: {
-		  // 필요하면 헤더 설정 (e.g., 인증 토큰)
-		},
-	  });
+            if (sortBy === 'latest') {
+                apiUrl = process.env.REACT_APP_API_URL + "/feed/";
+            } else if (sortBy === 'recommend') {
+                apiUrl = process.env.REACT_APP_API_URL + "/feed/recommend";
+            }
 
-	  if (!response.ok) {
-		throw new Error("API 요청이 실패했습니다.");
-	  }
+            const response = await fetch(apiUrl, {
+                method: "GET",
+                headers: {
+                    // 필요하면 헤더 설정 (e.g., 인증 토큰)
+                },
+            });
 
-	  const data = await response.json();
-	  setFeeds(data);
-	} catch (error) {
-	  console.error("API 요청 오류:", error);
-	}
-  }
+            if (!response.ok) {
+                throw new Error("API 요청이 실패했습니다.");
+            }
 
-  useEffect(() => {
-    fetchFeeds(); // 데이터 불러오기 함수 호출
-  }, []);
+            const data = await response.json();
+            setFeeds(data);
+        } catch (error) {
+            console.error("API 요청 오류:", error);
+        }
+    }
 
-  return (
-    <Layout>
-      <Row className="justify-content-evenly">
-        <Col sm={7}>
-          <Row className="border rounded align-items-center">
-            {(
-              <Col className="flex-shrink-1">
-              </Col>
-            )}
-            <Col sm={10} className="flex-grow-1">
-              <CreateFeed />
-            </Col>
-          </Row>
-          <Row className="my-4">
-            {feeds.map((feed, index) => (
-              <Feed key={index} feed={feed} refresh={fetchFeeds} />
-            ))}
-          </Row>
-        </Col>
-      </Row>
-    </Layout>
-  );
+    useEffect(() => {
+        fetchFeeds(); // 데이터 불러오기 함수 호출
+    }, [sortBy]);
+
+    return (
+        <Layout>
+            <Row className="justify-content-evenly">
+                <Col sm={7}>
+                    <Col sm={12}> {/* 전체 가로 공간을 사용할 열 */}
+                        <button onClick={() => setSortBy('latest')}>최신</button>
+                        <button onClick={() => setSortBy('recommend')}>추천순</button>
+                    </Col>
+                    <Row className="border rounded align-items-center">
+                        {(
+                            <Col className="flex-shrink-1">
+                            </Col>
+                        )}
+                        <Col sm={10} className="flex-grow-1">
+                            <CreateFeed/>
+                        </Col>
+                    </Row>
+                    <Row className="my-4">
+                        {feeds.map((feed, index) => (
+                            <Feed key={index} feed={feed} refresh={fetchFeeds}/>
+                        ))}
+                    </Row>
+                </Col>
+            </Row>
+        </Layout>
+    );
 }
 
 export default Home;
