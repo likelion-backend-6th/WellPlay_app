512d4a10396c0edf5958b9ce5f9138944ea2315a
bainaryho <rkswl233@naver.com>
2023-10-12 19:37:10 +0900

    [feat] 덧글 객체 스타일 저장 테스트

--- frontend/wp-front-app/src/App.js
+++ frontend/wp-front-app/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import ProtectedRoute from './routes/ProtectedRoute';
-import SinglePost from './pages/SinglePost';
 import Profile from './pages/Profile';
 import EditProfile from './pages/EditProfile';
 import FollowerList from "./components/follow/Follower";
@@ -12,14 +11,6 @@ function App() {
     return (
         <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route
-                path="/post/:postId/"
-                element={
-                    <ProtectedRoute>
-                        <SinglePost/>
-                    </ProtectedRoute>
-                }
-            />
             <Route
                 path="/profile/:profileId/"
                 element={
