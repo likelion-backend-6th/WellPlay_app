e5fdb62a4fe668e48242529d4b0f9416553a537b
Juwon Hwang <wndnjs719@gmail.com>
2023-10-13 10:49:08 +0900

    [feat] 피드에 로그인한 사용자 글 삭제 버튼 추가

--- frontend/wp-front-app/src/components/feeds/Feed.jsx
+++ frontend/wp-front-app/src/components/feeds/Feed.jsx
@@ -1,19 +1,20 @@
 import React, {useEffect, useState} from "react"
-import { Card, Image } from "react-bootstrap"
-import { format } from "timeago.js"
-import { CommentOutlined, LikeFilled, LikeOutlined } from "@ant-design/icons"
+import {Card, DropdownButton, Image, Dropdown} from "react-bootstrap"
+import {format} from "timeago.js"
+import {CommentOutlined, LikeFilled, LikeOutlined} from "@ant-design/icons"
 import axiosService from "../../helpers/axios"
-import { Link } from "react-router-dom"
+import {Link} from "react-router-dom"
 import {getUser, useUserActions} from "../../hooks/user.actions"
 
 function Feed(props) {
-	const { feed, refresh, isSingleFeed } = props
-	const user = getUser();
-	const {getUserProfile} = useUserActions();
+    const {feed, refresh, isSingleFeed} = props
+    const user = getUser();
+    const {getUserProfile} = useUserActions();
     const [profile, setProfile] = useState({});
     const userid = feed.user_id
+    const [showMenu, setShowMenu] = useState(false);
 
-	const fetchProfile = (userid) => {
+    const fetchProfile = (userid) => {
         getUserProfile(userid)
             .then((response) => {
                 setProfile(response.data);
@@ -22,104 +23,126 @@ function Feed(props) {
                 console.error('프로필 정보를 가져오는 중 오류 발생:', error);
             });
     };
-	useEffect(() => {
+    useEffect(() => {
         fetchProfile(userid);
     }, []);
 
-	const handleLikeClick = (action, data) => {
-		axiosService
-			.post(`/feed/${feed.id}/${action}/`, data)
-			.then(() => {
-				refresh()
-			})
-			.catch((err) => console.error(err))
-	}
+    const handleLikeClick = (action, data) => {
+        axiosService
+            .post(`/feed/${feed.id}/${action}/`, data)
+            .then(() => {
+                refresh()
+            })
+            .catch((err) => console.error(err))
+    }
+    const handleMenuClick = () => {
+        setShowMenu(!showMenu);
+    };
 
-	return (
-		<>
-			<Card className="rounded-3 my-4">
-				<Card.Body>
-					<Card.Title className="d-flex flex-row justify-content-between">
-						<div className="d-flex flex-row">
-							<Image
-								src={feed.profile_image}
-								roundedCircle
-								width={48}
-								height={48}
-								className="me-2 border border-dark
+    const handleDeleteClick = () => {
+        axiosService
+            .delete(`/feed/${feed.id}/`)
+            .then(() => {
+                refresh()
+            })
+            .catch((err) => console.error(err))
+    };
+
+    return (
+        <>
+            <Card className="rounded-3 my-4">
+                <Card.Body>
+                    <Card.Title className="d-flex flex-row justify-content-between">
+                        <div className="d-flex flex-row">
+                            <Image
+                                src={feed.profile_image}
+                                roundedCircle
+                                width={48}
+                                height={48}
+                                className="me-2 border border-dark
                           border-2"
-							/>
-							<div className="d-flex flex-column justify-content-start align-self-center mt-2">
-								<p className="fs-6 m-0">{profile.nickname}</p>
-								<p className="fs-6 fw-lighter">
-									<small>{format(feed.created_at)}</small>
-								</p>
-							</div>
-						</div>
-					</Card.Title>
-					<Card.Text>
-						{feed.content}
-						{feed.image_url && (
-							<Image
-								src={feed.image_url}
-								width="100%"
-								height="100%"
-								className="me-2 mb-3 border border-dark border-2"
-							/>
-						)}
-						{feed.video_url && (
-							<video
-								src={feed.video_url}
-								controls={true}
-								className="border border-dark border-2"
-								width="100%"
-								height="100%"
-								loop={false}
-								muted={false}
-							/>
-						)}
-					</Card.Text>
-				</Card.Body>
-				<Card.Footer className="d-flex bg-white w-50 justify-content-between border-0">
-					<div className="d-flex flex-row">
-						<LikeOutlined
-							style={{
-								width: "24px",
-								height: "24px",
-								padding: "2px",
-								fontSize: "20px",
-								color: feed.like ? "#0D6EFD" : "#C4C4C4",
-							}}
-							onClick={() => {
-								handleLikeClick("like", {"user": user.id, "feed": feed.id})
-							}}
-						/>
-						<p className="ms-1">
-							<small>{feed.like} Like</small>
-						</p>
-					</div>
-					{!isSingleFeed && (
-						<div className="d-flex flex-row">
-							<Link to={`/feed/${feed.id}/comments/`}>
-								<CommentOutlined
-									style={{
-										width: "24px",
-										height: "24px",
-										padding: "2px",
-										fontSize: "20px",
-										color: "#C4C4C4",
-									}}
-								/>
-							</Link>
-								<p className="ms-1 mb-0">
-									<small>{feed.comment} Comment</small>
-								</p>
-						</div>
-					)}
-				</Card.Footer>
-			</Card>
-		</>
-	)
+                            />
+                            <div className="d-flex flex-column justify-content-start align-self-center mt-2">
+                                <p className="fs-6 m-0">{profile.nickname}</p>
+                                <p className="fs-6 fw-lighter">
+                                    <small>{format(feed.created_at)}</small>
+                                </p>
+                            </div>
+                        </div>
+                        {user && user.user_id === feed.user_id && (
+                            <DropdownButton
+                                title="메뉴"
+                                id="menu-dropdown"
+                                show={showMenu}
+                                onClick={handleMenuClick}
+                            >
+                                <Dropdown.Item onClick={handleDeleteClick}>삭제</Dropdown.Item>
+                            </DropdownButton>
+                        )}
+                    </Card.Title>
+                    <Card.Text>
+                        {feed.content}
+                        {feed.image_url && (
+                            <Image
+                                src={feed.image_url}
+                                width="100%"
+                                height="100%"
+                                className="me-2 mb-3 border border-dark border-2"
+                            />
+                        )}
+                        {feed.video_url && (
+                            <video
+                                src={feed.video_url}
+                                controls={true}
+                                className="border border-dark border-2"
+                                width="100%"
+                                height="100%"
+                                loop={false}
+                                muted={false}
+                            />
+                        )}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer className="d-flex bg-white w-50 justify-content-between border-0">
+                    <div className="d-flex flex-row">
+                        <LikeOutlined
+                            style={{
+                                width: "24px",
+                                height: "24px",
+                                padding: "2px",
+                                fontSize: "20px",
+                                color: feed.like ? "#0D6EFD" : "#C4C4C4",
+                            }}
+                            onClick={() => {
+                                handleLikeClick("like", {"user": user.id, "feed": feed.id})
+                            }}
+                        />
+                        <p className="ms-1">
+                            <small>{feed.like} Like</small>
+                        </p>
+                    </div>
+                    {!isSingleFeed && (
+                        <div className="d-flex flex-row">
+                            <Link to={`/feed/${feed.id}/comments/`}>
+                                <CommentOutlined
+                                    style={{
+                                        width: "24px",
+                                        height: "24px",
+                                        padding: "2px",
+                                        fontSize: "20px",
+                                        color: "#C4C4C4",
+                                    }}
+                                />
+                            </Link>
+                            <p className="ms-1 mb-0">
+                                <small>{feed.comment} Comment</small>
+                            </p>
+                        </div>
+                    )}
+                </Card.Footer>
+            </Card>
+        </>
+    )
 }
 
 export default Feed
