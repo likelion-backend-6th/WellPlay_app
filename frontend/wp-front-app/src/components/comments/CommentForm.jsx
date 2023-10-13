7821fe507e2268a5982682d0ad0fd83e2560148b
binary_ho <90160898+bainaryho@users.noreply.github.com>
2023-10-13 18:27:35 +0900

    [feat] comment ver1.0 & desion feed comment (#93)
    * [feat] 피드 이미지, 영상크기 조절
    
    * [feat] 피드 div 나누기 & 이미지,영상 동적size 할당
    
    * [feat] 피드 이미지, 영상크기 조절
    
    * [feat] comment ver 0.1
    
    * [feat] call back 함수 덧글 새로고침
    
    * [feat] 덧글 객체 스타일 저장 테스트
    
    * [fix] 페이지 리로드로 에러처리
    
    * [fix] change commentSerializer
    
    * [feat] comment card
    
    * [feat] 피드리스트 디자인
    
    * [fix] Modal 크기 조정
    
    * [fix] Feed

---
+++ frontend/wp-front-app/src/components/comments/CommentForm.jsx
@@ -0,0 +1,37 @@
+// 덧글 작성하는 컴포넌트
+import React, { useState } from "react";
+import axiosService from "../../helpers/axios";
+
+function CommentForm({ feedId, onCommentPosted }) { // 여기에도 콜백 함수 적어주고
+  const [content, setContent] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const commentData = { content, feed: feedId };
+
+    axiosService
+      .post(`/feed/${feedId}/comments/`, commentData)
+      .then(() => {
+        onCommentPosted(); // 부모 컴포넌트에 새 댓글 게시를 알리기 위한 콜백 함수 호출
+        setContent("");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  return (
+    <form onSubmit={handleSubmit} style={{ display: "flex" }}>
+      <input
+        type="text"
+        placeholder="WOW Well Play!"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        style={{ marginRight: "10px" }}
+      />
+      <button type="submit">작성</button>
+    </form>
+  );
+}
+
+export default CommentForm;
--- frontend/wp-front-app/src/components/feeds/Feed.jsx
+++ frontend/wp-front-app/src/components/feeds/Feed.jsx
@@ -1,10 +1,11 @@
 import React, {useEffect, useState} from "react"
-import {Card, DropdownButton, Image, Dropdown, Button, Modal, Form} from "react-bootstrap"
+import {Card, DropdownButton, Image, Modal, Dropdown, Button, Form} from "react-bootstrap"
 import {format} from "timeago.js"
 import {CommentOutlined, LikeFilled, LikeOutlined} from "@ant-design/icons"
 import axiosService from "../../helpers/axios"
 import {Link} from "react-router-dom"
 import {getUser, useUserActions} from "../../hooks/user.actions"
+import CommentModal from "../comments/CommentModal";
 
 function Feed(props) {
     const {feed, refresh, isSingleFeed} = props
@@ -19,6 +20,7 @@ function Feed(props) {
     const data = new FormData();
 
     data.append('content', form.body);
+	const [showCommentModal, setShowCommentModal] = useState(false);
 
     const fetchProfile = (userid) => {
         getUserProfile(userid)
@@ -37,7 +39,7 @@ function Feed(props) {
         axiosService
             .post(`/feed/${feed.id}/${action}/`, data)
             .then(() => {
-                refresh()
+                refresh();
             })
             .catch((err) => console.error(err))
     }
@@ -159,25 +161,27 @@ function Feed(props) {
                         )}
                     </Card.Title>
                     <Card.Text>
-                        {feed.content}
+                        <div>
+                            {feed.content}
+                        </div>
                         {feed.image_url && (
                             <Image
-                                src={feed.image_url}
-                                width="100%"
-                                height="100%"
-                                className="me-2 mb-3 border border-dark border-2"
+                            src={feed.image_url}
+                            style={{ maxWidth: "100%", height: "400px", width:"auto" }}
+                            className="me-2 mb-3 border border-dark border-2"
                             />
                         )}
                         {feed.video_url && (
                             <video
-                                src={feed.video_url}
-                                controls={true}
-                                className="border border-dark border-2"
-                                width="100%"
-                                height="100%"
-                                loop={false}
-                                muted={false}
-                            />
+                            src={feed.video_url}
+                            style={{ maxWidth: "100%", height: "400px", width:"auto" }}
+                            controls={true}
+                            className="border border-dark border-2"
+                            width="100%"
+                            height="100%"
+                            loop={false}
+                            muted={false}
+                        />
                         )}
                     </Card.Text>
                 </Card.Body>
@@ -200,18 +204,25 @@ function Feed(props) {
                         </p>
                     </div>
                     {!isSingleFeed && (
-                        <div className="d-flex flex-row">
-                            <Link to={`/feed/${feed.id}/comments/`}>
-                                <CommentOutlined
-                                    style={{
-                                        width: "24px",
-                                        height: "24px",
-                                        padding: "2px",
-                                        fontSize: "20px",
-                                        color: "#C4C4C4",
-                                    }}
-                                />
-                            </Link>
+						<div className="d-flex flex-row">
+						<CommentOutlined
+							style={{
+							width: "24px",
+							height: "24px",
+							padding: "2px",
+							fontSize: "20px",
+							color: "#C4C4C4",
+							cursor: !user ? "not-allowed" : "pointer",
+							}}
+							onClick={() => {
+								if (!user) {
+									window.location.reload();
+									alert("로그인이 필요합니다");
+								  } else {
+									setShowCommentModal(true);
+								  }
+							}}
+						/>
                             <p className="ms-1 mb-0">
                                 <small>{feed.comment} Comment</small>
                             </p>
@@ -219,6 +230,13 @@ function Feed(props) {
                     )}
                 </Card.Footer>
             </Card>
+            <CommentModal
+					feedId={feed.id}
+					show={showCommentModal}
+					size="lg" //크게만들고싶은데
+					handleClose={() => setShowCommentModal(false)}
+					//refreshComments={/* 함수를 호출하여 덧글 목록 업데이트 */}
+			/>
         </>
     )
 }
