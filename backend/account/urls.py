3ee81aa436221a07388163b3343147e0f91f2b54
binary_ho <90160898+bainaryho@users.noreply.github.com>
2023-10-17 10:30:30 +0900

    [feat] 이메일 인증 가입 모듈 설계 및 구현 (#105)

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("riot_summoner_info/<str:user_id>/", InfololList.as_view()),  # GET
     path("riot_val_info/", riot_val_info),  # POST
     path("riot_val_info/<str:user_id>/", InfovalList.as_view()),  # GET
+    path("activate/<str:uidb64>/<str:token>/", activate_account, name="activate"),
 ]
