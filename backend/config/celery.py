34c48e6ed6567a4395a07daba9700d3a9315c40b
JunYoung_Kim <hjklo0220@gmail.com>
2023-10-17 17:38:25 +0900

    [fix] add celery Dockerfile & fixed script

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
