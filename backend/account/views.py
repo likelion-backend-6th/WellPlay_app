8fb2596c4b349c17827ec2102b8159dab4c99b8c
JunYoung_Kim <43990500+hjklo0220@users.noreply.github.com>
2023-10-18 18:20:46 +0900

    [feat] add backend testcode (#120)
    * [feat] add feed testcode
    
    * [style] account.view import
    
    * [feat] add account testcode
    
    * [fix] fix feed testcode

--- backend/account/views.py
+++ backend/account/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import authenticate
 from django.core.files import File
 from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import extend_schema
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
@@ -10,20 +9,15 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework import views
 from celery.result import AsyncResult
-from django.core.mail import send_mail
-from django.urls import reverse
 from django.contrib.auth import get_user_model
-from django.utils.http import urlsafe_base64_decode
-import base64
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 
 from .serializers import *
-from account.tasks import *  # Celery 작업 import
+from account.tasks import *
 from common.uploader import FileUploader
 
 
--- backend/feed/tests.py
+++ backend/feed/tests.py
@@ -1,7 +1,9 @@
-from rest_framework.test import APITestCase
-from rest_framework.test import APIClient
+import json
+import tempfile
+from unittest.mock import patch, MagicMock
+
+from rest_framework.test import APITestCase, APIClient
 from rest_framework.response import Response
-from rest_framework.request import Request
 from rest_framework import status
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model
@@ -20,17 +22,134 @@ class FeedTest(APITestCase):
         cls.authorized_user2 = cls.test_model.objects.create_user(
             email="auth2@admin.com", user_id="auth2", password="1234"
         )
-        cls.unauthorized_user = cls.test_model.objects.create_user(
-            email="unauth@admin.com", user_id="unauth", password="1234"
-        )
         cls.post_data = {
             "content": "test content",
             "owner": cls.authorized_user1,
         }
 
-    # 피드 수정, 삭제
-    def test_feed_update_delete(self):
-        client = APIClient()
-        client.force_authenticate(user=self.authorized_user1)
-        res: Response = client.post("/feed/", self.post_data)
+    # 피드 조회, 작성, 수정, 삭제
+    def test_feed_crud(self):
+        login_client = APIClient()
+        login_client.force_authenticate(user=self.authorized_user1)
+        # get
+        res: Response = login_client.get("/feed/")
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        # post
+        res: Response = login_client.post("/feed/", self.post_data)
+        feed_id = res.data["id"]
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        # put
+        res: Response = login_client.put(
+            f"/feed/{feed_id}/", {"content": "test content update"}
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        # another user put
+        login_client.force_authenticate(user=self.authorized_user2)
+        res: Response = login_client.put(
+            f"/feed/{feed_id}/", {"content": "another user update"}
+        )
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+        # another user delete
+        res: Response = login_client.delete(f"/feed/{feed_id}/")
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+        # delete
+        login_client.force_authenticate(user=self.authorized_user1)
+        res: Response = login_client.delete(f"/feed/{feed_id}/")
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+
+    @patch("common.uploader.boto3.client")
+    def test_post_with_or_without_image(self, s3_client: MagicMock):
+        login_client = APIClient()
+        login_client.force_authenticate(user=self.authorized_user1)
+        # mock s3
+        s3 = MagicMock()
+        s3_client.return_value = s3
+        s3.upload_fileobj.return_value = None
+        s3.put_object_acl.return_value = None
+
+        data = {
+            "content": "test",
+        }
+
+        res: Response = login_client.post("/feed/", data)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        with tempfile.NamedTemporaryFile(suffix=".jpg") as f:
+            data["image"] = f
+            res: HttpResponse = login_client.post("/feed/", data=data)
+            self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+            res_data = json.loads(res.content)
+            self.assertTrue(res_data["image_url"].startswith("https://"))
+
+        s3.upload_fileobj.assert_called_once()
+        s3.put_object_acl.assert_called_once()
+
+    def test_comment_crud(self):
+        login_client = APIClient()
+        login_client.force_authenticate(user=self.authorized_user1)
+        # post
+        res: Response = login_client.post("/feed/", self.post_data)
+        feed_id = res.data["id"]
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        # post comment
+        res: Response = login_client.post(
+            f"/feed/{feed_id}/comments/", {"content": "test comment"}
+        )
+        comment_id = res.data["id"]
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        # get
+        res: Response = login_client.get(f"/feed/{feed_id}/comments/")
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        # put comment
+        res: Response = login_client.put(
+            f"/feed/{feed_id}/comments/{comment_id}/",
+            {"content": "test comment update"},
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+        # another user put
+        login_client.force_authenticate(user=self.authorized_user2)
+        res: Response = login_client.put(
+            f"/feed/{feed_id}/comments/{comment_id}/",
+            {"content": "another user update"},
+        )
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+        # another user delete
+        res: Response = login_client.delete(f"/feed/{feed_id}/comments/{comment_id}/")
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+        # delete
+        login_client.force_authenticate(user=self.authorized_user1)
+        res: Response = login_client.delete(f"/feed/{feed_id}/comments/{comment_id}/")
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_like(self):
+        login_client = APIClient()
+        login_client.force_authenticate(user=self.authorized_user1)
+        # write feed
+        res: Response = login_client.post("/feed/", self.post_data)
+        feed_id = res.data["id"]
+        user_id = self.authorized_user1.id
+
+        like_data = {
+            "user": user_id,
+            "feed": feed_id,
+        }
+
+        # like
+        res: Response = login_client.post(f"/feed/{feed_id}/like/", like_data)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+        # unlike
+        res: Response = login_client.post(f"/feed/{feed_id}/like/", like_data)
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
